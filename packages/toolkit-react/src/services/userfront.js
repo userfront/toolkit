650e5cde68b1284cfe7e948849e7f4976b82a308
Ryan Felix <ryan@userfront.com>
2022-12-05 19:13:19 -0500

    Remove c.log

--- packages/toolkit-react/src/services/userfront.js
+++ packages/toolkit-react/src/services/userfront.js
@@ -18,13 +18,9 @@ const call = async ({ method, args, isDevMode = false, _devDataType = "" }) => {
   return new Promise((resolve, reject) => {
     const service = interpret(machine).onTransition((state) => {
       if (state.matches("success") || state.matches("successDev")) {
-        console.log("success");
-        console.log(state.context);
         return resolve(state.context.result);
       }
       if (state.matches("failure")) {
-        console.log("failure");
-        console.log(state.context);
         return reject(state.context.error);
       }
     });
