bca0af0a7fac740386e38625a9188bf160468e3f
Ryan Felix <ryan@userfront.com>
2023-01-04 22:06:11 -0500

    Add workaround for Vite/Rollup issue with react-phone-input-2 in production

--- packages/toolkit-react/src/components/PhoneNumberInput.jsx
+++ packages/toolkit-react/src/components/PhoneNumberInput.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
-import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
+
+// Workaround for a Vite/Rollup issue where CJS module exports
+// work in development mode but not in production mode (?!?)
+// See https://github.com/vitejs/vite/issues/2139#issuecomment-1173976668
+
+import RPI from "react-phone-input-2";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const PhoneInput = RPI.default ?? RPI;
 
 const PhoneNumberInput = ({ ...props }) => {
   const [number, setNumber] = useState("");
